b100c228cf879f104f2859b5e58ee59057f44b4f
Mark Olliver <mark@digitalpatterns.io>
2018-09-10 15:01:47 +0100

    updated to make more sane

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 [[ -n ${DEBUG} ]] && set -x
+
+# Expected Files
+# /certs/tls.pem - Server cert - if missing create
+# /certs/tls-key.pem - Server key - if missing create
+# /etc/ssl/certs/ca.pem - CA Cert (or intermediate CA)
+# /etc/ssl/certs/ca-bundle.crt - CA Bundle
+
+CERTIFICATE_FILE="${CERTIFCATE_FILE:-/certs/tls.pem}"
+PRIVATE_KEY_FILE="${PRIVATE_KEY_FILE:-/certs/tls-key.pem}"
 
 IMPORT_SYSTEM_TRUSTSTORE=${IMPORT_SYSTEM_TRUSTSTORE:-"true"}
 JAVA_CACERTS=${JAVA_CACERTS:-"/etc/ssl/java/cacerts"}
@@ -11,7 +20,7 @@ TRUSTSTORE_FILE=${TRUSTSTORE_FILE:-"${KEYSTORE_RUNTIME}/truststore.jks"}
 # step: create the keystore runtime
 mkdir -p ${KEYSTORE_RUNTIME}
 
-annonce() {
+announce() {
   [ -n "$@" ] && echo "[v] --> $@"
 }
 
@@ -20,15 +29,15 @@ failed() {
 }
 
 create_truststore() {
-  annonce "Creating a JAVA truststore as ${TRUSTSTORE_FILE}"
+  announce "Creating a JAVA truststore as ${TRUSTSTORE_FILE}"
   if [ -f "${CA_CERT_FILE}" ]; then
-    annonce "Importing the CA ${CA_CERT_FILE} into the keystore"
+    announce "Importing the CA ${CA_CERT_FILE} into the keystore"
     keytool -import -alias ca -file ${CA_CERT_FILE} -keystore ${TRUSTSTORE_FILE} \
       -noprompt -storepass changeit -trustcacerts
   fi
 
   if [[ ${IMPORT_SYSTEM_TRUSTSTORE} == 'true' ]]; then
-    annonce "Importing ${JAVA_CACERTS} into ${TRUSTSTORE_FILE}."
+    announce "Importing ${JAVA_CACERTS} into ${TRUSTSTORE_FILE}."
     keytool -importkeystore -destkeystore ${TRUSTSTORE_FILE} \
       -srckeystore ${JAVA_CACERTS} -srcstorepass changeit \
       -noprompt -storepass changeit &> /dev/null
@@ -36,11 +45,11 @@ create_truststore() {
 }
 
 create_keystore() {
-  annonce 'Creating a temporary pkcs12 keystore.'
+  announce "Creating a temporary pkcs12 keystore."
   openssl pkcs12 -export -name cert -in ${CERTIFICATE_FILE} -inkey ${PRIVATE_KEY_FILE} -nodes \
     -CAfile ${CA_CERT_FILE} -out ${KEYSTORE_RUNTIME}/keystore.p12 -passout pass: || failed "unable to convert certificates pkcs12 format"
 
-  annonce "Creating a JAVA keystore as ${KEYSTORE_FILE}."
+  announce "Creating a JAVA keystore as ${KEYSTORE_FILE}."
   keytool -importkeystore -destkeystore ${KEYSTORE_FILE} \
     -srckeystore ${KEYSTORE_RUNTIME}/keystore.p12 -srcstoretype pkcs12 \
     -alias cert -srcstorepass '' -noprompt -storepass changeit || failed "unanle to import the pkcs12 into keystore"
@@ -48,15 +57,19 @@ create_keystore() {
   keytool -keypasswd -new changeit -keystore ${KEYSTORE_FILE} -storepass changeit -alias cert -keypass ''
 }
 
-# step: the vault-sidekick will pass the file=<filename> or the name.type, we are ASSUMING the ca, key and cert are in .ca, .key and .crt
-CERTIFICATE_FILE=${1}
-PRIVATE_KEY_FILE=${2}
-CA_CERT_FILE=${1}
-[[ -n "${3}" ]] && CA_CERT_FILE="${3}"
+create_certs() {
+    announce "Create Certificates."
+    [ -z "${DOMAIN}" ] || failed "Certificate Domain name not supplied"
+    /cfssl-sidekick --certs=/certs --expiry=8760h --domain=${DOMAIN}
+}
 
 # step: at the very least we must have cert and private key
-[ -f "${CERTIFICATE_FILE}" ] || failed "cannot find the certificate file: ${CERTIFICATE_FILE}"
-[ -f "${PRIVATE_KEY_FILE}" ] || failed "cannot find the private key file: ${PRIVATE_KEY_FILE}"
-
-create_truststore
-create_keystore
+if [[ -f "${CERTIFICATE_FILE}" ]] && [[ -f "${PRIVATE_KEY_FILE}" ]]
+then
+    create_truststore
+    create_keystore
+else
+    create_certs
+    create_truststore
+    create_keystore
+fi
